fd45952bd90d9cb8b5e81531f86b95d612d67bcf
MASTERS <anusara.orm@gmail.com>
2017-11-25 13:50:27 +0700

    test add main.cpp delete

---
+++ app/src/main/java/com/example/masters/fs88htcpp/testMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.masters.fs88htcpp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class testMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_main);
+    }
+}
